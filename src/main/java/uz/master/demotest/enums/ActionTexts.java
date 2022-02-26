f88ed62dea613b3e2cdf640379090b5d81a671cd
Shoxruh2002 <91327698+Shoxruh2002@users.noreply.github.com>
2022-02-26 22:10:21 +0500

    project entity ga teamLeader Id sini quwdm,task ni update,delete qiladi, agar teamleaderi bulsa bumasa 405 exeptioan tawlaydi

--- src/main/java/uz/master/demotest/enums/ActionTexts.java
+++ src/main/java/uz/master/demotest/enums/ActionTexts.java
@@ -12,7 +12,10 @@ public enum ActionTexts {
     USER_JOINED("New user joined to task"),
     USER_REMOVED("User removed from task"),
     TASK_COLUMN_UPDATED("Task Column updated"),
-    TASK_UPDATED("Task Updated");
+    TASK_UPDATED("Task Updated"),
+    TASK_PRIORITY_UPDATED("Task Priority Updated"),
+    NEW_COMMIT("New commit wrote"),
+    TASK_LEVEL_UPDATED("Task Level Updated");
 
     private String text;
 }
--- src/main/java/uz/master/demotest/services/task/TaskService.java
+++ src/main/java/uz/master/demotest/services/task/TaskService.java
@@ -3,17 +3,18 @@ package uz.master.demotest.services.task;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import uz.master.demotest.configs.security.UserDetails;
-import uz.master.demotest.dto.auth.AuthDto;
 import uz.master.demotest.dto.task.TaskCreateDto;
 import uz.master.demotest.dto.task.TaskDto;
 import uz.master.demotest.dto.task.TaskUpdateDto;
 import uz.master.demotest.entity.action.Action;
 import uz.master.demotest.entity.auth.AuthUser;
+import uz.master.demotest.entity.column.ProjectColumn;
 import uz.master.demotest.entity.task.Task;
 import uz.master.demotest.entity.task.Task_Member;
 import uz.master.demotest.enums.ActionTexts;
 import uz.master.demotest.mappers.TaskMapper;
 import uz.master.demotest.repositories.AuthUserRepository;
+import uz.master.demotest.repositories.ColumnRepository;
 import uz.master.demotest.repositories.TaskRepository;
 import uz.master.demotest.services.AbstractService;
 import uz.master.demotest.services.GenericCrudService;
@@ -22,6 +23,7 @@ import uz.master.demotest.utils.Validator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService extends AbstractService<TaskRepository, TaskMapper, Validator>
@@ -30,10 +32,12 @@ public class TaskService extends AbstractService<TaskRepository, TaskMapper, Val
 
 
     private final AuthUserRepository authUserRepository;
+    private final ColumnRepository columnRepository;
 
-    protected TaskService(TaskRepository repository, TaskMapper mapper, TaskValidator validator, AuthUserRepository authUserRepository) {
+    protected TaskService(TaskRepository repository, TaskMapper mapper, TaskValidator validator, AuthUserRepository authUserRepository, ColumnRepository columnRepository) {
         super(repository, mapper, validator);
         this.authUserRepository = authUserRepository;
+        this.columnRepository = columnRepository;
     }
 
     @Override
@@ -61,7 +65,9 @@ public class TaskService extends AbstractService<TaskRepository, TaskMapper, Val
 
     @Override
     public Void update(TaskUpdateDto dto) {
-        repository.update(dto.getId(), dto.getName(), dto.getDescription(), dto.getTaskOrder(), dto.getLevel(), dto.getPriority(), dto.getColumnId());
+        UserDetails userDetails = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        repository.addAction(dto.getId(), userDetails.getUsername(), ActionTexts.TASK_UPDATED.getText());
+        repository.update(dto.getId(), dto.getName(), dto.getDescription(), dto.getTaskOrder(), dto.getLevel(), dto.getPriority());
         return null;
     }
 
@@ -80,11 +86,13 @@ public class TaskService extends AbstractService<TaskRepository, TaskMapper, Val
     }
 
     public Void addMamber(Long taskId, Long memberId) {
+        repository.addAction(taskId, authUserRepository.findById(memberId).get().getUsername(), ActionTexts.USER_JOINED.getText());
         repository.addMember(taskId, memberId);
         return null;
     }
 
     public Void deleteMember(Long taskId, Long memberId) {
+        repository.addAction(taskId, authUserRepository.findById(memberId).get().getUsername(), ActionTexts.USER_REMOVED.getText());
         repository.deleteMember(taskId, memberId);
         return null;
     }
@@ -103,15 +111,24 @@ public class TaskService extends AbstractService<TaskRepository, TaskMapper, Val
     }
 
     public void updatePriority(Long id, String code) {
+
         repository.updatePriority(id, code);
-        repository.addAction(id, ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername(), ActionTexts.TASK_UPDATED.getText());
+        repository.addAction(id, ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername(), ActionTexts.TASK_PRIORITY_UPDATED.getText());
     }
 
     public void updateLevel(Long id, String code) {
         repository.updateLevel(id, code);
+        repository.addAction(id, ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername(), ActionTexts.TASK_LEVEL_UPDATED.getText());
     }
 
     public void joinTask(Long id) {
-        repository.addMember(id, ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+        UserDetails userDetails = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        repository.addAction(id, userDetails.getUsername(), ActionTexts.USER_JOINED.getText());
+        repository.addMember(id, userDetails.getId());
+    }
+
+    public Long getProjectId(Long id) {
+        Task task = repository.findById(id).get();
+        return columnRepository.findById(task.getColumnId()).get().getProjectId();
     }
 }
