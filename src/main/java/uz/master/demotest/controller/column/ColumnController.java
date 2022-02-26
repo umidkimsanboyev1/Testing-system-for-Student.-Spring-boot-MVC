0820377f2125a8d4154258427b4e18e28c050d27
Shoxruh2002 <91327698+Shoxruh2002@users.noreply.github.com>
2022-02-26 16:09:05 +0500

    magic

--- src/main/java/uz/master/demotest/controller/column/ColumnController.java
+++ src/main/java/uz/master/demotest/controller/column/ColumnController.java
@@ -2,54 +2,50 @@ package uz.master.demotest.controller.column;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import uz.master.demotest.dto.column.ColumnCreateDto;
-import uz.master.demotest.dto.column.ColumnDto;
 import uz.master.demotest.dto.column.ColumnUpdateDto;
 import uz.master.demotest.services.column.ColumnService;
+import uz.master.demotest.services.project.ProjectService;
 import uz.master.demotest.services.task.TaskService;
 
-import java.util.List;
-
 @Controller
-@RequestMapping("project/column/")
+@RequestMapping("/project/column/")
 public class ColumnController {
     private final ColumnService service;
+    private final ProjectService projectService;
 
 
     public ColumnController(
             ColumnService service,
-            TaskService taskService) {
+            TaskService taskService, ProjectService projectService) {
         this.service = service;
 
+        this.projectService = projectService;
     }
 
     @RequestMapping("list/{id}")
-    public String list(Model model, @RequestParam Long id) {
-
-
+    public String list(Model model, @PathVariable(name = "id") Long id) {
+        model.addAttribute("project", projectService.get(id));
         model.addAttribute("columns", service.getAll(id));
-        return "index2";
+        return "project/project";
     }
 
     @RequestMapping(value = "create/", method = RequestMethod.POST)
     public String create(@ModelAttribute ColumnCreateDto dto) {
         service.create(dto);
-        return "redirect:index2";
+        return "redirect:/project/project";
     }
 
     @RequestMapping(value = "update/", method = RequestMethod.POST)
     public String create(@ModelAttribute ColumnUpdateDto dto) {
         service.update(dto);
-        return "redirect:index2";
+        return "redirect:/project/project";
     }
 
     @RequestMapping(value = "delete/", method = RequestMethod.DELETE)
     public String delete(@RequestParam Long id) {
         service.delete(id);
-        return "redirect:index2";
+        return "redirect:/project/project";
     }
 }
